254aa96a1571383c5f791917d910fe442b42d920
Piotr Proszowski <215165@edu.p.lodz.pl>
2018-03-01 22:53:40 +0100

    First warm-up task

---
+++ TrinomialSquareCalculator/TrinomialSquareCalculator.java
@@ -0,0 +1,42 @@
+public class TrinomialSquareCalculator{
+    public static void main(String[] args){
+        if(args.length < 3){
+            System.out.println("Not enough arguments");
+            return;
+        }
+        try{
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+
+            double delta = Math.pow(b,2) - 4 * a * c;
+
+            if(a == 0){
+                if(b == 0){
+                    if(c == 0){
+                        System.out.println("This equation has infinite number of solutions");
+                    }else{
+                        System.out.println("This equation has not any solution");
+                    }
+                }else{
+                    double solution = -c/b;
+                    System.out.println("This equation has only one solution, x = " + solution);
+                }
+                return;
+            }
+
+            if(delta < 0){
+                System.out.println("There is no solution in interval of real numbers for passed parameters");
+            }else if(delta == 0){
+                int solution = -b/(2*a);
+                System.out.println("This equation has only one solution, x = " + solution);
+            }else{
+                double solution1 = (-b - Math.sqrt(delta))/(2*a);
+                double solution2 = (-b + Math.sqrt(delta))/(2*a);
+                System.out.println("This equation has two solutions, x1 = " + solution1 + ", x2 = " + solution2);
+            }
+        }catch(NumberFormatException e){
+            System.out.println("Passed arguments should be integers");
+        }
+    }
+}
