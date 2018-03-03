dde1acb75e4cac25a77e2684ebf62455ed8ca0d7
Piotr Proszowski <215165@edu.p.lodz.pl>
2018-03-03 11:17:41 +0100

    first version of StringCutter

---
+++ StringCutter/StringCutter.java
@@ -0,0 +1,22 @@
+public class StringCutter{
+    public static void main(String[] args){
+        if(args.length < 3){
+            System.out.println("At least 3 arguments should be provided");
+            return;
+        }
+
+        try{
+            int begin = Integer.parseInt(args[0]);
+            int end = Integer.parseInt(args[1]) + 1;
+            if(end < begin){
+                System.out.println("Second integer (endIndex) should be less than first integer (beginIndex)");
+            }
+            String str = args[2];
+            System.out.println(str.substring(begin, end));
+        }catch(NumberFormatException e){
+            System.out.println("Two first arguments should be integers");
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Second integer should be less than string length and first should greater or equal 0\nIndex out of bounds exception");
+        }
+    }
+}
