89b970d495df9126dc9acda84042456804aa6726
Piotr Proszowski <215165@edu.p.lodz.pl>
2018-03-14 21:30:17 +0100

    Break code to functions

--- TrinomialSquareCalculator/TrinomialSquareCalculator.java
+++ TrinomialSquareCalculator/TrinomialSquareCalculator.java
@@ -12,30 +12,38 @@ public class TrinomialSquareCalculator{
             double delta = Math.pow(b,2) - 4 * a * c;
 
             if(a == 0){
-                if(b == 0){
-                    if(c == 0){
-                        System.out.println("This equation has infinite number of solutions");
-                    }else{
-                        System.out.println("This equation does not have any solution");
-                    }
-                }else{
-                    double solution = -c/b;
-                    System.out.println("This equation has only one solution, x = " + solution);
-                }
+                solveLinearEquation(b,c);
             }else{
-                if(delta < 0){
-                    System.out.println("There is no solution in interval of real numbers for passed parameters");
-                }else if(delta == 0){
-                    int solution = -b/(2*a);
-                    System.out.println("This equation has only one solution, x = " + solution);
-                }else{
-                    double solution1 = (-b - Math.sqrt(delta))/(2*a);
-                    double solution2 = (-b + Math.sqrt(delta))/(2*a);
-                    System.out.println("This equation has two solutions, x1 = " + solution1 + ", x2 = " + solution2);
-                }
+                solveTrinomialSquareEquation(a, b, c);
             }
         }catch(NumberFormatException e){
             System.out.println("Passed arguments should be integers");
+        }
+    }
+
+    private static void solveLinearEquation(int b, int c){
+        if(b == 0){
+            if(c == 0){
+                System.out.println("This equation has infinite number of solutions");
+            }else{
+                System.out.println("This equation does not have any solution");
+            }
+        }else{
+            double solution = -c/b;
+            System.out.println("This equation has only one solution, x = " + solution);
+        }
+    }
+
+    private static void solveTrinomialSquareEquation(int a, int b, int c){
+        if(delta < 0){
+            System.out.println("There is no solution in interval of real numbers for passed parameters");
+        }else if(delta == 0){
+            int solution = -b/(2*a);
+            System.out.println("This equation has only one solution, x = " + solution);
+        }else{
+            double solution1 = (-b - Math.sqrt(delta))/(2*a);
+            double solution2 = (-b + Math.sqrt(delta))/(2*a);
+            System.out.println("This equation has two solutions, x1 = " + solution1 + ", x2 = " + solution2);
         }
     }
 }
